3020547dfb357aeacdc39c76fa330b385e29dfe6
oly-min <liberosm0818@naver.com>
2022-05-31 16:38:59 +0900

    model learning(5 epochs) - using Kkma

--- 02_crawled_data_concat.py
+++ 02_crawled_data_concat.py
@@ -12,13 +12,13 @@ df = pd.DataFrame()
 for path in data_path[0:]:
     df_temp = pd.read_csv(path)
     df = pd.concat([df, df_temp])
-#혹시 있을지도 모르는 NaN값 제거
-df.dropna(inplace=True)
-df.reset_index(inplace=True, drop=True) #index있는 거 합칠때 drop=True가 필수
+
+# #혹시 있을지도 모르는 NaN값 제거
+# df.dropna(inplace=True)
+# df.reset_index(inplace=True, drop=True) #index있는 거 합칠때 drop=True가 필수
 
 # print(df['분류'].value_counts())
 # df.info()
 
 # csv파일 저장
-
 df.to_csv('./crawled_data/joonggo_luxury_items_concat_{}.csv'.format(datetime.datetime.now().strftime('%Y%m%d')), index=False)
--- 05_model_predict.py
+++ 05_model_predict.py
@@ -12,15 +12,15 @@ from tensorflow.keras.models import load_model
 
 pd.set_option('display.unicode.east_asian_width', True)
 pd.set_option('display.max_columns', 20)    #20column까지는 모두 출력
-df = pd.read_csv('./crawling_data/naver_headline_news_20220527.csv')
+df = pd.read_csv('./crawled_data/joonggo_test_data.csv')
 # print(df.head())
 # df.info()
 
 #라벨링
-X = df['제목']  #입력
+X = df['제목']    #입력
 Y = df['분류']  #타겟 9개
 
-#저장해놓은 pickled encoder 불러오기
+#저장해놓은 encoder 불러오기
 with open('./models/encoder.pickle', 'rb') as f:
     encoder = pickle.load(f)
 
@@ -30,11 +30,11 @@ label = encoder.classes_
 
 #onehot encoding (가변수화)
 onehot_Y = to_categorical(labeled_Y)
-print(onehot_Y)
+# print(onehot_Y)
 
 #X 전처리
 okt = Okt() #openkoreantokenizer: 피팅 후 딕셔너리 형태
-# okt_morph_X = okt.morphs(X[7], stem=True)  #morphs: 형태소 단위로 잘라주는 함수 / stem=True: 어근으로 변환해줌
+okt_morph_X = okt.morphs(X[7], stem=True)  #morphs: 형태소 단위로 잘라주는 함수 / stem=True: 어근으로 변환해줌
 # print(okt_morph_X)  #형태소 리스트
 
 for i in range(len(X)):
@@ -42,7 +42,7 @@ for i in range(len(X)):
 # print(X[:10])
 
 #불용어 dataframe 만들기
-stopwords = pd.read_csv('crawling_data/stopwords.csv', index_col=0)   #0번 column이 인덱스다!
+stopwords = pd.read_csv('./crawled_data/stopwords.csv', index_col=0)   #0번 column이 인덱스다!
 # print(stopwords.head())
 
 #불용어 제거하고 남은 형태소 리스트 만들기
@@ -58,56 +58,62 @@ for i in range(len(X)):
                 words.append(X[i][j])
     X[i] = ' '.join(words)
 
-#저장해놓은 token 불러오기
-with open('./models/news_token.pickle', 'rb') as f:
+#저장해놓은 tokenizer 불러오기
+with open('./models/joonggo_token.pickle', 'rb') as f:
     token = pickle.load(f)
 
 tokened_X = token.texts_to_sequences(X)
-print(tokened_X[:5])
+# print(tokened_X[:5])
 
-#tokened_X 속 max값을 가진 요소 찾기
+# #tokened_X 속 max값을 가진 요소 찾기
 for i in range(len(tokened_X)):
-    if len(tokened_X[i]) > 17:
-        tokened_X[i] = tokened_X[i][:17]
+    if len(tokened_X[i]) > 32:
+        tokened_X[i] = tokened_X[i][:32]
 
 #padding
-X_pad = pad_sequences(tokened_X, 17)
-print((X_pad[:5]))
+X_pad = pad_sequences(tokened_X, 32)
+# print((X_pad[:5]))
 
 #모델 불러오기
-model = load_model('./models/news_category_classification_model_0.7087541818618774.h5')
+model = load_model('./models/joonggo_category_classification_model_0.8580850958824158_okt.h5')
 preds = model.predict(X_pad)
 
 #예측 max값의 column 리스트 만들기
 predicts = []
 for pred in preds:
     most = label[np.argmax(pred)]   #확률 max값의 label
-    pred[np.argmax(pred)] = 0
-    second = label[np.argmax(pred)] #두번째로 큰 확률의 label
-    predicts.append([most, second])
+    predicts.append(most)
 df['predict'] = predicts
 
-print(df.head(30))
+# predicts = []
+# for pred in preds:
+#     most = label[np.argmax(pred)]   #확률 max값의 label
+#     pred[np.argmax(pred)] = 0
+#     second = label[np.argmax(pred)] #두번째로 큰 확률의 label
+#     predicts.append([most, second])
+# df['predict'] = predicts
+
+# print(df.head(30))
 
 
 #예측 적중 여부 확인
 df['OX'] = 0
 for i in range(len(df)):
-    if df.loc[i, 'category'] in df.loc[i, 'predict']:
+    if df.loc[i, '분류'] in df.loc[i, 'predict']:
         df.loc[i, 'OX'] = 'O'
     else:
         df.loc[i, 'OX'] = 'X'
-print(df.head(30))
-
+# print(df.head(30))
 
 #정답률 확인
 print(df['OX'].value_counts())  #정답수
 print(df['OX'].value_counts()/len(df))  #정답률
 
+print()
 #예측 실패 출력
-for i in range(len(df)):
-    if df['category'][i] not in df['predict'][i]:
-        print(df.iloc[i])
+# for i in range(len(df)):
+#     if df['분류'][i] not in df['predict'][i]:
+#         print(df.iloc[i])
 
 
 
